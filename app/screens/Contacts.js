1f8a9f782b07d03b3282c24def355a808087a012
Zak <lysakada@gmail.com>
2017-08-04 18:13:21 +0200

    Real Start File

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -3,16 +3,17 @@ import React, {Component} from 'react';
 import {View, Text, FlatList} from 'react-native'; // Pour créer l'écran.
 import {contacts} from '../config/data.js';
 import colors from'../config/colors'; // On importe la liste de contact de notre config.
+import{ListItem} from '../components/ListItem';
 
 class Contacts extends Component{
     render(){
         return(
-
             <FlatList
             style={{backgroundColor:colors.background}}
             data={contacts}
             renderItem={({ item }) =>
-            <View><Text>{item.login.username}</Text></View>}
+                <ListItem contact={item} onPress={()=>this.handleRowPress(item)}/>
+            }
             keyExtractor={(item) =>item.email}
             />
         );
