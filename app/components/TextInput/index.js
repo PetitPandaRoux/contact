e8b0b9d34048aa4c59c15e0564370822cf8fbf9f
Zak <lysakada@gmail.com>
2017-08-08 15:26:30 +0200

    Me & NewContact completed

---
+++ app/components/TextInput/index.js
@@ -0,0 +1,4 @@
+import TextInput from './TextInput.js';
+import styles from './styles.js';
+
+export {TextInput, styles}
--- app/screens/NewContact.js
+++ app/screens/NewContact.js
@@ -1,14 +1,41 @@
 //En premier lieu nous important des éléments de react et react-native
 import React, {Component} from 'react';
-import {View, Text} from 'react-native'; // Pour créer l'écran.
+import {ScrollView, Text} from 'react-native'; // Pour créer l'écran.
+import colors from '../config/colors';
+import {TextInput} from '../components/TextInput';
+
+const fields=[
+    {placeholder:'First Name', stateKey: 'firstName'},
+    {placeholder:'Last Name', stateKey: 'lastName'},
+    {placeholder:'Email', stateKey:'email', keyboardType:"email-address"},
+];
+
 
 class NewContact extends Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    onInputChange=(text,stateKey) =>{
+        const mod={};
+        mod[stateKey]=text;
+        this.setState(mod);
+    }
     render(){
         return(
-            <View
-            style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>NewContact Screen</Text>
-            </View>
+            <ScrollView
+                style={{backgroundColor:colors.background}}>
+                {
+                    fields.map((field)=> (
+                        <TextInput
+                            key={field.stateKey}
+                            onChange={(text)=>this.onInputChange (text,field.stateKey )}
+                            {...field}
+                        />
+                    ))
+                }
+            </ScrollView>
         );
     }
 }
